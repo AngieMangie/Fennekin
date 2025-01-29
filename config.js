0945b2be6bb8d89cb85c1be9c5d84b54b68a0437
AngieMangie <99761717+AngieMangie@users.noreply.github.com>
2025-01-29 17:34:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["743649979385774091", ""], 
-  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority ",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
@@ -14,7 +14,7 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-  {
+     {
   "identifier": "Public Lavalink v4 NonSSL",
   "password": "https://dsc.gg/ajidevserver",
   "host": "lava-v4.ajieblogs.eu.org",
